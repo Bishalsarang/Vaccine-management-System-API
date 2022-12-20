03d7a49142f63979e0c12efd622571633cbcd1ec
Bishalsarang <bishalsarang@gmail.com>
2022-12-20 16:28:21 +0545

    Add basic chores: swagger, cors, global prefix.

---
+++ src/constant/base.constant.ts
@@ -0,0 +1,2 @@
+export const URL_PREFIX = 'api/v1';
+export const SWAGGER_URL = 'swagger-ui';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+
+import * as cors from 'cors';
+
 import { AppModule } from './app.module';
+import { SWAGGER } from './constant/lang.constant';
+import { SWAGGER_URL, URL_PREFIX } from './constant/base.constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // configure cors
+  app.use(cors());
+
+  // Configure Swagger
+  configureSwagger(app);
+
+  // Enable the validation pipe
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.setGlobalPrefix(URL_PREFIX);
+
   await app.listen(3000);
 }
+
+/**
+ * Configures the Swagger documentation and testing tool for the specified NestJS application.
+ *
+ * @param {INestApplication} app - The NestJS application to configure Swagger for.
+ */
+function configureSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle(SWAGGER.title)
+    .setDescription(SWAGGER.description)
+    .setVersion(SWAGGER.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup(SWAGGER_URL, app, document);
+}
+
 bootstrap();
